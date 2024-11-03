c494a5e4aa68d886bbad67554b71d800db8425e2
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-03 23:27:14 +0200

    change files: main.js, 6Qb97gsv.html

---
+++ Lesson1/6Qb97gsv/main.js
@@ -0,0 +1,9 @@
+let firstName = 'Іван';
+let middleName = 'Іванович';
+let lastName = 'Іванов';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let person_2 = `${firstName} ${middleName} ${lastName}`;
+console.log(person_2);
