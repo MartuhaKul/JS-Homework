6820f0f38b50489b2c97ef718022635dbd590bb6
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-03 23:15:45 +0200

    change file main.js

---
+++ Lesson1/main.js
@@ -0,0 +1,39 @@
+let greeting = 'hello';
+console.log(greeting);
+
+let country = 'owu';
+console.log(country);
+
+let domain = 'com';
+console.log(domain);
+
+let tld = 'ua';
+console.log(tld);
+
+let num_1 = 1;
+console.log(num_1);
+
+let num_2 = 10;
+console.log(num_2);
+
+let negative_num = -999;
+console.log(negative_num);
+
+let positive_num = 123;
+console.log(positive_num);
+
+const PI = 3.14;
+console.log(PI);
+
+const e = 2.7;
+console.log(e);
+
+let num_3 = 16;
+console.log(num_3);
+
+let True = true;
+console.log(True);
+
+let False = false;
+console.log(False);
+
