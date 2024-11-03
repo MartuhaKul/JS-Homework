b5972f5600a5ef938bc627b5e1d8d87c30995f90
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-03 23:41:34 +0200

    change files: main.js, ruUtWDUI.html

---
+++ Lesson1/ruUtWDUI/main.js
@@ -0,0 +1,7 @@
+let user_Name = prompt("Введіть своє ім'я:");
+let user_MiddleName = prompt("Введіть своє по-батькові:");
+let user_Age = prompt("Введіть свій вік:");
+
+console.log(user_Name);
+console.log(user_MiddleName);
+console.log(user_Age);
