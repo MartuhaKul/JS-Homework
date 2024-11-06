6f93547ecfd59437d164f6c69fe2898ab91f521b
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-06 16:03:00 +0200

    change files: main.js, uwsz1RnTQJ1.html

---
+++ Lesson2/uwsz1RnTQJ1/main.js
@@ -0,0 +1,10 @@
+let num1 = 7;
+let num2 = 7;
+
+if (num1 > num2) {
+    console.log('Максимальне число:' +num1);
+}else if (num1 < num2) {
+    console.log('Максимальне число:' +num2);
+}else {
+    console.log('Числа рівні')
+}
