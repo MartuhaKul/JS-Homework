d6c0af2266c82f394d9774b10260ceb83e1dd320
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-06 14:13:16 +0200

    change files: main.js, LARqoUj5I.html

---
+++ Lesson2/LARqoUj5I/main.js
@@ -0,0 +1,16 @@
+let book1 = {
+    title: 'Assistant',
+    pageCount: 366,
+    genre: 'thriller'
+}
+let book2 = {
+    title: 'The fourth wing',
+    pageCount: 524,
+    genre: 'fantasy'
+}
+let book3 = {
+    title: 'Murder on the Orient Express',
+    pageCount: 288,
+    genre: 'detective'
+}
+console.log(book1, book2, book3);
