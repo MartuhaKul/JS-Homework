96030a21c18b0ad3ffa404a26306d173a148f6d2
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-06 14:27:51 +0200

    change files: main.js, sA3Gg1sCp.html

---
+++ Lesson2/sA3Gg1sCp/main.js
@@ -0,0 +1,35 @@
+let book1 = {
+    title: 'Assistant',
+    pageCount: 366,
+    genre: 'thriller',
+    author:[
+        {
+        name: 'Tess Gerritsen',
+        age: 71
+    }
+    ]
+}
+let book2 = {
+    title: 'The fourth wing',
+    pageCount: 524,
+    genre: 'fantasy',
+    author:[
+        {
+        name: 'Rebecca Yarros',
+        age: 43
+    }
+    ]
+}
+let book3 = {
+    title: 'Murder on the Orient Express',
+    pageCount: 288,
+    genre: 'detective',
+    author:[
+        {
+        name: 'Agatha Christie',
+        age: 85
+    }
+    ]
+}
+
+console.log(book1, book2, book3)
