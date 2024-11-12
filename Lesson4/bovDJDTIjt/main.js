314a8eb32bb7727e521430bac9f703fe2ba725b5
MartuhaKul <martysichkaaa15@gmail.com>
2024-11-12 17:34:00 +0200

    change files: main.js, bovDJDTIjt.html

---
+++ Lesson4/bovDJDTIjt/main.js
@@ -0,0 +1,13 @@
+function arrayOfUsers(users) {
+    for (const user of users) {
+        document.write(`<div>${user.id} ${user.username} ${user.age}</div>`);
+    }
+}
+arrayOfUsers([
+    {id: 1, username: 'kokosik', age: 12},
+    {id: 2, username: 'pepsi', age: 10},
+    {id: 3, username: 'lapka', age: 15},
+    {id: 4, username: 'bysya', age: 12},
+    {id: 5, username: 'pechenka', age: 11}
+
+])
